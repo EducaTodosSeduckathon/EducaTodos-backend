947ca76d037de6e1734760394611cd3eed4074d3
Kayck L Brito <kaycklbr@gmail.com>
2025-06-24 09:31:25 -0300

    first commit

---
+++ models/Turma.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/sequelize');
+
+const Turma = sequelize.define('Turma', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [1, 100],
+    },
+  },
+  school_year: {
+    type: DataTypes.STRING(10),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  section: {
+    type: DataTypes.STRING(10),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+}, {
+  tableName: 'turmas',
+  timestamps: true,
+  indexes: [
+    {
+      unique: true,
+      fields: ['school_year', 'section'],
+    },
+  ],
+});
+
+module.exports = Turma;
+
